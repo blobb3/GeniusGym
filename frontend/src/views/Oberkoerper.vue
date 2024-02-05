1c3c236fe4fdc5b4dc391bcad3ccf3decf4675a6
blobb3 <janine.heinemeyer@gmail.com>
2024-02-05 19:58:14 +0100

    GIFS für Oberkörpertraining

--- frontend/src/views/Oberkoerper.vue
+++ frontend/src/views/Oberkoerper.vue
@@ -1,43 +1,51 @@
 <template>
     <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Oberkörpertraining</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content :fullscreen="true">
-        <ion-accordion-group :multiple="true">
-          <ion-accordion value="first">
-            <ion-item slot="header" color="light">
-              <ion-label>Liegestütze</ion-label>
-            </ion-item>
-            <div class="ion-padding" slot="content">
-              Stärkt Brust, Schultern und Trizeps. 2 Sätze von je 10-15 Wiederholungen.
-            </div>
-          </ion-accordion>
-          <ion-accordion value="second">
-            <ion-item slot="header" color="light">
-              <ion-label>Klimmzüge</ion-label>
-            </ion-item>
-            <div class="ion-padding" slot="content">
-              Zielt auf die Rückenmuskulatur und den Bizeps. 2 Sätze von je 8-12 Wiederholungen.
-            </div>
-          </ion-accordion>
-          <ion-accordion value="third">
-            <ion-item slot="header" color="light">
-              <ion-label>Schulterdrücken</ion-label>
-            </ion-item>
-            <div class="ion-padding" slot="content">
-              Kräftigt die Schultermuskulatur. 2 Sätze von je 10-15 Wiederholungen.
-            </div>
-          </ion-accordion>
-        </ion-accordion-group>
-      </ion-content>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Oberkörpertraining</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-accordion-group :multiple="true">
+                <ion-accordion value="first">
+                    <ion-item slot="header" color="light">
+                        <ion-label>Liegestütze</ion-label>
+                    </ion-item>
+                    <div class="ion-padding" slot="content">
+                        <img src="https://www.fitundattraktiv.de/wp-content/uploads/2018/02/liegestuetze_muskelaufbau-liegestuetze_klassisch.gif"
+                            style="max-width: 360px; height: auto; margin: 0 auto; display: block;">
+                        Stärkt Brust, Schultern und Trizeps. 2 Sätze von je 10-15 Wiederholungen.
+                    </div>
+
+                </ion-accordion>
+                <ion-accordion value="second">
+                    <ion-item slot="header" color="light">
+                        <ion-label>Klimmzüge</ion-label>
+                    </ion-item>
+                    <div class="ion-padding" slot="content">
+                        <img src="https://homeworkouts.org/wp-content/uploads/anim-rear-pull-ups.gif"
+                            style="max-width: 360px; height: auto; margin: 0 auto; display: block;">
+                        Zielt auf die Rückenmuskulatur und den Bizeps. 2 Sätze von
+                    </div>
+                </ion-accordion>
+
+                <ion-accordion value="third">
+  <ion-item slot="header" color="light">
+    <ion-label>Schulterdrücken</ion-label>
+  </ion-item>
+  <div class="ion-padding" slot="content">
+    <img src="https://modusx.de/wp-content/uploads/schulterdruecken-kurzhanteln-sitzend.gif" style="max-width: 360px; height: auto; margin: 0 auto; display: block;">
+    Kräftigt die Schultermuskulatur. 2 Sätze von je 10-15 Wiederholungen.
+  </div>
+</ion-accordion>
+
+            </ion-accordion-group>
+        </ion-content>
     </ion-page>
-  </template>
+</template>
   
-  <script setup lang="ts">
-  import {
+<script setup lang="ts">
+import {
     IonPage,
     IonHeader,
     IonToolbar,
@@ -47,12 +55,12 @@
     IonAccordion,
     IonItem,
     IonLabel,
-  } from '@ionic/vue';
-  </script>
+} from '@ionic/vue';
+</script>
 
-  <style>
+<style>
 .ion-padding {
-    color:black;
+    color: black;
 }
 </style>
   
