466b1ac7d0d8df2f0811863eb70e47374c302393
GeorgeGrecko <deliggeo@students.zhaw.ch>
2024-02-06 10:49:28 +0100

    Update Map

--- frontend/src/views/RunningMap.vue
+++ frontend/src/views/RunningMap.vue
@@ -1,41 +1,41 @@
 <template>
-    <ion-page>
-        <ion-header>
-            <ion-toolbar>
-                <ion-title>Wähle deine Joggingstrecke</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content :fullscreen="true" class="ion-padding">
-            <ion-button expand="block" @click="generateRoute">Generiere eine Strecke in deiner Nähe</ion-button>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Wähle deine Joggingstrecke</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-button expand="block" @click="generateRoute">Generiere eine Strecke in deiner Nähe</ion-button>
 
-            <!-- Hier könnte ein Bild einer Karte eingefügt werden -->
-            <div class="map-container">
-                <img src="https://www.raonline.ch/images/hike/pic7/swisstopo_karten18005.jpg" style="max-width: 684px; height: 351px; margin: 0px; width: 437px;">
-            </div>
+      <div class="map-container">
+        <div id="map" style="height: 350px; width: 100%;"></div>
+      </div>
 
-            <div class="timer-buttons">
-                <ion-button @click="startTimer">Start</ion-button>
-                <ion-button @click="stopTimer">Ziel</ion-button>
-                <ion-button @click="resetTimer">Reset</ion-button>
-            </div>
+      <div class="timer-buttons">
+        <ion-button @click="startTimer">Start</ion-button>
+        <ion-button @click="stopTimer">Ziel</ion-button>
+        <ion-button @click="resetTimer">Reset</ion-button>
+      </div>
 
-            <!-- Stoppuhr Anzeige -->
-            <div class="timer-display">
-                Zeit: {{ elapsedTime }}
-            </div>
-        </ion-content>
-    </ion-page>
+      <!-- Stoppuhr Anzeige -->
+      <div class="timer-display">
+        Zeit: {{ elapsedTime }}
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import {
-    IonPage,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonButton,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
 } from '@ionic/vue';
+
 
 // Verwenden refs für den Startzeitpunkt, die verstrichene Zeit und das Intervall
 let startTime = ref<Date | null>(null);
@@ -79,25 +79,51 @@ const resetTimer = () => {
   elapsedTime.value = '00:00:00';
   startTime.value = null; // Startzeit zurücksetzen
 };
+
+//Map
+declare global {
+  interface Window { initMap: () => void; }
+}
+
+window.initMap = function() {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // Überprüfe, ob mapElement nicht null ist
+    const map = new google.maps.Map(mapElement as HTMLElement, {
+      center: { lat: -34.397, lng: 150.644 }, // Beispielkoordinaten, anpassen nach Bedarf
+      zoom: 8
+    });
+  } else {
+    console.error('Map container not found');
+  }
+};
+
+onMounted(() => {
+  console.log(import.meta.env.VITE_GOOGLE_MAPS_API_KEY)
+  const script = document.createElement('script');
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+  script.async = true;
+  script.defer = true;
+  document.head.appendChild(script);
+});
 </script>
 
   
 <style scoped>
 .map-container img {
-    width: 100%;
-    margin: 16px 0;
+  width: 100%;
+  margin: 16px 0;
 }
 
 .timer-buttons {
-    display: flex;
-    justify-content: space-around;
-    margin: 16px 0;
+  display: flex;
+  justify-content: space-around;
+  margin: 16px 0;
 }
 
 .timer-display {
-    text-align: center;
-    margin-top: 16px;
-    color: black;
+  text-align: center;
+  margin-top: 16px;
+  color: black;
 }
 
 .ion-padding {
@@ -107,6 +133,5 @@ const resetTimer = () => {
   align-items: center;
   text-align: center;
 }
-
 </style>
   
