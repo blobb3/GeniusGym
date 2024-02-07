f068d88d2aed7145aecc44d22fd57172dc8871aa
blobb3 <janine.heinemeyer@gmail.com>
2024-02-07 20:38:17 +0100

    tests

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
-
 import { IonicVue } from '@ionic/vue';
+import { useStore } from './store'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -23,10 +23,12 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+const app = createApp(App);
+
+app.use(IonicVue);
+app.use(router);
+app.use(useStore);
+
 router.isReady().then(() => {
   app.mount('#app');
 });
